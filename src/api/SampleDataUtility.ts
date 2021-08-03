baf9c623af7c2a4e6830ce903fbc7687e2272185
Noah Lee <noahlee@Noahs-MacBook-Pro.local>
2021-08-03 14:45:01 -0400

    add sample data

---
+++ src/api/SampleDataUtility.ts
@@ -0,0 +1,11 @@
+import * as Media from '../sample-data/media.json';
+import * as User from '../sample-data/user.json';
+import * as PostMetadata from '../sample-data/post-metadata.json';
+import * as Review from '../sample-data/review.json';
+import * as ScoringSystem from '../sample-data/scoring-system.json';
+
+export const getMedia = () => Media;
+export const getUser = () => User;
+export const getPostMetadata = () => PostMetadata;
+export const getReview = () => Review;
+export const getScoringSystem = () => ScoringSystem;
